fa70acd8feaadd6bd194a647801c3f8e89193956
zohidjonkarimov01-tech <zohidjonkarimov01@gmail.com>
2026-09-06 20:17:42 +0500

    Add files via upload

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,37 @@
+// let
+// let name = "Mars";
+// name = "Mercury";
+// console.log(name);
+
+
+// var
+// var name ="Mars";
+// name = "Saturn";
+// var name = "Mercury";
+// console.log(name);
+
+
+// const
+
+// const name = "Mercury";
+// const age = 12;
+// console.log(name);
+// console.log("Your name")
+// console.log(`Your name ${name}`);
+
+
+const name = prompt("Ismingizni kiriting");
+const surname = +prompt("Familiyangizni kiriting");
+const age = +prompt("Yoshingizni kiriting");
+const best = +prompt("Yoqtirgan faningizni yozing");
+const lokation = +prompt("Yashaydigon tumaningizni kiriting");
+const text = confirm("Malumotingizni kiritganingiz uchun tashakkur");
+console.log(`Ism: ${name} \nFamiliya: ${surname} \nYosh: ${age} \nYoqtirgan fan: ${best} \nYashash manzili: ${lokation}`);
+
+
+
+
+
+
+
+ 
